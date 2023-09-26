e529d02f64dbf9fe9b178ee77f8b172a1e95f349
Jefferson Marinho <marinho@localhost.localdomain>
2023-09-25 22:09:51 -0300

    feat: handler errors and returns

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -4,54 +4,82 @@ import (
 	"api/src/banco"
 	"api/src/modelos"
 	"api/src/repositorios"
+	"api/src/router/respostas"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"strings"
 )
 
-//insere usuario no banco
+// insere usuario no banco
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	corpoRequest, erro := ioutil.ReadAll(r.Body)
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
 	}
 	var usuario modelos.Usuario
 	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = usuario.Preparar(); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
 	}
 
 	db, erro := banco.Conectar()
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
 
 	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
-	usuarioID, erro := repositorio.Criar(usuario)
+	usuario.ID, erro = repositorio.Criar(usuario)
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
 
-	w.Write([]byte(fmt.Sprintf("Id inserido: %d", usuarioID)))
+	respostas.JSON(w, http.StatusCreated, usuario)
+
 }
 
-//busca todos usuarios
+// busca todos usuarios
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("buscando usuarios!"))
+	nomeOuNick := strings.ToLower(r.URL.Query().Get("usuario"))
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarios, erro := repositorio.Buscar(nomeOuNick)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, usuarios)
+
 }
 
-//busca um usuario
+// busca um usuario
 func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("buscando usuario!"))
 }
 
-//altera informações de um usuario
+// altera informações de um usuario
 func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("atualizando usuario!"))
 }
 
-//excluir as informações de um usuario
+// excluir as informações de um usuario
 func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("deletando usuario!"))
 }
--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -3,6 +3,7 @@ package repositorios
 import (
 	"api/src/modelos"
 	"database/sql"
+	"fmt"
 )
 
 type Usuarios struct {
@@ -16,7 +17,7 @@ func NovoRepositorioDeUsuarios(db *sql.DB) *Usuarios {
 
 // criar insere um usuario no banco de dados
 func (repositorio Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
-	statement, erro := repositorio.db.Prepare("insert into usuarios(nome, nick, email, senha) value (?,?,?,?)",)
+	statement, erro := repositorio.db.Prepare("insert into usuarios(nome, nick, email, senha) value (?,?,?,?)")
 	if erro != nil {
 		return 0, erro
 	}
@@ -27,8 +28,40 @@ func (repositorio Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
 	}
 
 	ultimoIDInserido, erro := resultado.LastInsertId()
-	if erro != nil {return 0, erro}
+	if erro != nil {
+		return 0, erro
+	}
 
 	return uint64(ultimoIDInserido), nil
 
+}
+
+func (repositorio Usuarios) Buscar(nomeOuNick string) ([]modelos.Usuario, error) {
+	nomeOuNick = fmt.Sprintf("%%%s", nomeOuNick)
+
+	linhas, erro := repositorio.db.Query("select id, nome, nick, email, criadoEm from usuarios where nome LIKE ? or nick LIKE ?", nomeOuNick, nomeOuNick)
+
+	if erro != nil {
+		return nil, erro
 	}
+
+	defer linhas.Close()
+
+	var usuarios []modelos.Usuario
+
+	for linhas.Next() {
+		var usuario modelos.Usuario
+		if erro = linhas.Scan(
+			&usuario.ID,
+			&usuario.Nome,
+			&usuario.Nick,
+			&usuario.Email,
+			&usuario.CriadoEm,
+		); erro != nil {
+			return nil, erro
+		}
+		usuarios = append(usuarios, usuario)
+	}
+	return usuarios, nil
+
+}
